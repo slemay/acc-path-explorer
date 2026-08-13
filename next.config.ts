e7694abb93ec9418daadd25f75270a4938919c4e
slemay <slemay05@gmail.com>
2026-08-13 13:29:11 -0500

    ci: configure GitHub Actions deployment for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, '') || 'acc-path-explorer';
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: basePath,
+  assetPrefix: assetPrefix,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
